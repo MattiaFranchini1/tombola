62b29f85ca456abd0d7c727c44a45b0fd8076bb1
Mattia Franchini <101174838+MattiaFranchini1@users.noreply.github.com>
2023-09-29 19:11:35 +0200

    Update server.js

--- server.js
+++ server.js
@@ -20,24 +20,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log(__dirname)
 
-/*
-import express from 'express';
-
-const app = express();
-
-app.listen(3000, () =>
-  console.log('Example app listening on port 3000!'),
-);
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
-})
-
-app.get('/script.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '/script.js'));
-})
-
-*/
 wss.on('connection', function connection(ws) {
   if (partita_iniziata == true) {
     ws.send(JSON.stringify({ "accettato": false }));
