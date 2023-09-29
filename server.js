bac964f7e2d81903a23d2179c9db1f5596dad38f
Mattia Franchini <101174838+MattiaFranchini1@users.noreply.github.com>
2023-09-29 17:54:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+
 import { WebSocketServer } from "ws";
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-
+/*
 let clients_socket = []
 let num_utenti = 0;
 let cart = [];
@@ -15,7 +16,7 @@ let ambo = false, terna = false, quaterna = false, cinquina = false, tombola = f
 const wss = new WebSocketServer({ port: 8080 });
 console.log(wss);
 
-
+*/
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 console.log(__dirname)
@@ -25,7 +26,7 @@ import express from 'express';
 
 const app = express();
 
-app.listen(8080, () =>
+app.listen(3000, () =>
   console.log('Example app listening on port 3000!'),
 );
 
@@ -37,6 +38,7 @@ app.get('/script.js', (req, res) => {
   res.sendFile(path.join(__dirname, '/script.js'));
 })
 
+/*
 wss.on('connection', function connection(ws) {
   if (partita_iniziata == true) {
     ws.send(JSON.stringify({ "accettato": false }));
@@ -319,3 +321,5 @@ function rincomincia() {
   nomi_utenti = [];
   ambo = false, terna = false, quaterna = false, cinquina = false, tombola = false;
 }
+
+  */
